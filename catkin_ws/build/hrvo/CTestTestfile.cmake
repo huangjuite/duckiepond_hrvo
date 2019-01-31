8543abaa07f7c64b0c4b18832eab7cf7c3685fae
huangjuite <huangjuite@gmail.com>
2019-01-31 21:11:28 +0800

    remove /build /devel

--- catkin_ws/build/hrvo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ray/duckiepond_hrvo/catkin_ws/src/hrvo
-# Build directory: /home/ray/duckiepond_hrvo/catkin_ws/build/hrvo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
