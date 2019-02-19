454c5df879031ffb7641a146718937be28ba108f
huangjuite <huangjuite@gmail.com>
2019-02-20 03:09:42 +0800

    motion planing

--- catkin_ws/src/hrvo/src/demo_node.py
+++ catkin_ws/src/hrvo/src/demo_node.py
@@ -8,18 +8,25 @@ from RVO import RVO_update, reach, compute_V_des, reach
 from PID import PID_control
 from dynamic_reconfigure.server import Server
 from control.cfg import ang_PIDConfig,dis_PIDConfig
+import math
 import tf
+
 
 class BoatHRVO(object):
     def __init__(self):
         self.node_name = rospy.get_name()
         rospy.loginfo("[%s] Initializing" %self.node_name)
 
+        #initiallize PID
         self.dis_pid = [PID_control("distance_control%d" % i) for i in range(4)]
         self.angu_pid = [PID_control("angular_control%d" % i) for i in range(4)]
         self.dis_server = Server(dis_PIDConfig,self.cb_dis_pid,"distance_control")
         self.ang_server = Server(ang_PIDConfig,self.cb_ang_pid,"angular_control")
+        for i in range(4):
+            self.dis_pid[i].setSampleTime(0.1)
+            self.angu_pid[i].setSampleTime(0.1)
 
+        #setup publisher
         self.pub_v1 = rospy.Publisher("/boat1/cmd_drive",UsvDrive,queue_size=1)
         self.sub_p3d1 = rospy.Subscriber("/boat1/p3d_odom",Odometry,self.cb_boat1_odom,queue_size=1)
 
@@ -32,54 +39,102 @@ class BoatHRVO(object):
         self.pub_v4 = rospy.Publisher("/boat4/cmd_drive",UsvDrive,queue_size=1)
         self.sub_p3d1 = rospy.Subscriber("/boat4/p3d_odom",Odometry,self.cb_boat4_odom,queue_size=1)
 
-        self.boat_odom = []
-        for i in range(4):
-            self.boat_odom.append(Odometry())
+        #initiallize boat status
+        self.boat_odom = [Odometry() for i in range(4)]
+        self.cmd_drive = [UsvDrive() for i in range(4)]
+        self.yaw = [0 for i in range(4)]
 
-        self.yaw = [[0] for i in range(4)]
 
+        #initiallize HRVO environment
         self.ws_model = dict()
         #robot radius
         self.ws_model['robot_radius'] = 1.5
         self.ws_model['circular_obstacles'] = []
-        #rectangular boundary, format [
-        #x,y,width/2,heigth/2]
+        #rectangular boundary, format [x,y,width/2,heigth/2]
         self.ws_model['boundary'] = [] 
 
         self.pin1 = [7.5,7.5]
         self.pin2 = [-7.5,7.5]
         self.pin3 = [-7.5,-7.5]
         self.pin4 = [7.5,-7.5]
-        self.position = [self.pin1] + [self.pin2] + [self.pin3] + [self.pin4]
+        self.position = []
         self.goal = [self.pin3] + [self.pin4] + [self.pin1] + [self.pin2]
         #print(self.position)
         #print(self.goal)
-        self.velocity = [[0,0] for i in xrange(len(self.position))]
-        self.v_max = [1 for i in xrange(len(self.position))]
-        self.R = 1
-        self.L = 1
+        self.velocity = [[0,0] for i in range(4)]
+        self.v_max = [2 for i in range(4)]
 
+        #timer
         self.timer = rospy.Timer(rospy.Duration(0.2),self.cb_hrvo)
 
 
     def cb_hrvo(self,event):
-        self.pos_update()
+        self.update_all()
         v_des = compute_V_des(self.position,self.goal,self.v_max)
         self.velocity = RVO_update(self.position,v_des,self.velocity,self.ws_model)
         #print("position",self.position)
         #print("velocity",self.velocity)
 
-    def pos_update(self):
+        for i in range(4):
+            dis , angle = self.process_ang_dis(self.velocity[i][0],self.velocity[i][1],self.yaw[i])
+            self.dis_pid[i].update(5*dis)
+            self.angu_pid[i].update(angle)
+            dis_out = max(min(self.dis_pid[i].output,1),-1) * -1
+            ang_out = max(min(self.angu_pid[i].output,1),-1)
+            self.cmd_drive[i] = self.control_cmd_drive(dis_out, ang_out)
+
+        self.pub_v1.publish(self.cmd_drive[0])
+        self.pub_v2.publish(self.cmd_drive[1])
+        self.pub_v3.publish(self.cmd_drive[2])
+        self.pub_v4.publish(self.cmd_drive[3])
+
+    def control_cmd_drive(self,dis,angle):
+        cmd = UsvDrive()
+        cmd.left = max(min(dis-angle,1),-1)
+        cmd.left = max(min(dis+angle,1),-1)
+        return cmd
+
+    def process_ang_dis(self,vx,vy,yaw):
+        dest_yaw = math.atan2(vy,vx)
+        if dest_yaw > yaw:
+            right_yaw = dest_yaw-yaw
+            left_yaw = (2*math.pi-right_yaw)*-1
+        else:
+            left_yaw = dest_yaw-yaw
+            right_yaw = (2*math.pi-(left_yaw*-1))
+        #-1 < angle < 1 , find close side to turn
+        angle = left_yaw if abs(left_yaw) < abs(right_yaw) else right_yaw
+        angle = angle/math.pi
+        dis = 1 - abs(angle)
+
+        dis = max(min(dis,1),-1)
+        angle = max(min(angle,1),0)
+        return dis , angle
+
+    def update_all(self):
         self.position = []
         for i in range(4):
+            #update position
             pos = [self.boat_odom[i].pose.pose.position.x,self.boat_odom[i].pose.pose.position.y]
             self.position.append(pos)
+
+            #update orientation
             quaternion = (self.boat_odom[i].pose.pose.orientation.x,
                         self.boat_odom[i].pose.pose.orientation.y,
                         self.boat_odom[i].pose.pose.orientation.z,
                         self.boat_odom[i].pose.pose.orientation.w)
             euler = tf.transformations.euler_from_quaternion(quaternion)
             self.yaw[i] = euler[2]
+            
+            #update velocity
+            self.velocity[i] = [self.boat_odom[i].twist.twist.linear.x,
+                                self.boat_odom[i].twist.twist.linear.y]
+            
+            print("\nboat%d" % i)
+            print(self.position[i])
+            print(self.yaw[i])
+            print(self.velocity[i])
+            
 
     def cb_boat1_odom(self,msg):
         self.boat_odom[0] = msg
